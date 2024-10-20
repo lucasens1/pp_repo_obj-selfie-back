6de182086870f39a8fe1be788a1eede2d9ff1688
Luca Sensini <157043499+lucasens1@users.noreply.github.com>
2024-10-20 16:20:54 +0200

    layoutting

--- obj_selfie-back-end/resources/views/admin/dashboard.blade.php
+++ obj_selfie-back-end/resources/views/admin/dashboard.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
     {{-- Welcome Back --}}
-    
-                    @if (session('status'))
-                        <div class="" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h1 class="letter-spacing">
-                        @php
-                            $username = ucfirst(strtolower(Auth::user()->name));
-                        @endphp
-                        {{ __('Benvenuto') }} {{ $username }} {{ __('!') }}<br>
-                        {{ __('Ecco il tuo pannello di controllo:') }}
-                    </h1>
+    <div class="border border-1 w-1/2">
+        @if (session('status'))
+            <div class="" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        <h1>
+            @php
+                $username = ucfirst(strtolower(Auth::user()->name));
+            @endphp
+            Benvenuto {{ $username }}!
+        </h1>
+        <h2> Cosa vuoi fare? </h2>
+    </div>
+    <div class="border border-1 w-1/2">
+        <ul>
+            <li>Vediamo quanti messaggi hai ricevuto : {{-- Inserimento dati dinamici dei messaggi ricevuti --}}</li>
+            <li>Qui puoi vedere quanti ti hanno contattato nell'ultimo mese : {{-- Inserimento dinamico di messaggi ricevuti nell'ultimo mese Mese Corrente - Inizio Mese per il calcolo --}}</li>
+            <li>Hai <i>N</i> {{-- Numero di messaggi con isRead a false --}}Messaggi non letti ! </li>
+        </ul>
+    </div>
     {{-- / Welcome --}}
 @endsection
--- obj_selfie-back-end/resources/views/layouts/app.blade.php
+++ obj_selfie-back-end/resources/views/layouts/app.blade.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="">
-        <div class="bg-slate-500">
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat esse, qui ducimus quidem commodi sequi consectetur autem eum numquam, optio dolore! Officia, et accusamus impedit voluptatum ipsam alias minima aliquam.
-           Commodi quae velit, expedita voluptas optio doloribus non quas alias ipsum facilis et exercitationem inventore consequatur at eos modi officia ex in esse qui magnam? Nihil deleniti sit facere fugiat.
-           Laudantium harum eaque nulla, illum possimus animi soluta quidem doloremque tenetur dolores earum, numquam velit quos, voluptates magni. Accusamus cumque nobis praesentium aspernatur, labore voluptatem illum facere vero quidem nostrum.
-           Architecto quas id aliquam in, vitae ipsam inventore, soluta tenetur ea quod placeat quisquam veniam? Atque pariatur earum aliquid nam? Tenetur eligendi ex, provident molestias eveniet doloremque officia. Cupiditate, delectus?
-           Dolore aut officiis maiores repudiandae dolores eum? Minima quo placeat suscipit similique animi. Quod commodi repellat ad quidem cupiditate modi saepe dolore doloribus repudiandae voluptas esse, illo vero, rerum tenetur?
-           Vel laudantium voluptatem a eligendi quibusdam voluptate veniam voluptas. Doloribus deserunt recusandae perspiciatis tempora debitis minus in, dolor eveniet quae consequatur quo vero similique aspernatur, voluptatum enim officiis magni quod?
-            
-           @yield('content')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-           
-        </div>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <nav class="bg-slate-500">
+        <ul class="flex justify-evenly px-6 py-4 text-white">
+            <li><a href="#">Tutti i messaggi</a></li>
+            <li><a href="#">Da Leggere</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+
+
+    </nav>
+    <main class="flex">
+        @yield('content')
+    </main>
+</body>
+
 </html>
