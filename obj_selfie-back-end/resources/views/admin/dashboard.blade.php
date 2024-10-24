8f5da5a7834f6873104e1211bf597a5c32fa3ab7
Luca Sensini <157043499+lucasens1@users.noreply.github.com>
2024-10-24 14:20:54 +0200

    Fixing minor issues

--- obj_selfie-back-end/resources/views/admin/dashboard.blade.php
+++ obj_selfie-back-end/resources/views/admin/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     {{-- Welcome Back --}}
-    <div class="border border-1 w-1/2 max-sm:w-full">
+    <div class="border border-1 w-1/2 max-sm:w-full p-4">
         {{-- @if (session('status'))
             <div class="" role="alert">
                 {{ session('status') }}
@@ -11,23 +11,45 @@
         @php
             $username = ucfirst(strtolower(Auth::user()->name));
         @endphp
-        <h1>Benvenuto {{ $username }}!</h1>
+        <h1>Benvenuto <strong>{{ $username }}</strong>!</h1>
         <h2>Cosa vuoi fare?</h2>
     </div>
-    <div class="border border-1 w-1/2 max-sm:w-full">
-        <ul>
-            <li>Vediamo quanti messaggi hai ricevuto : {{ $messages->count()}} {{-- Inserimento dati dinamici dei messaggi ricevuti --}}</li>
-            <li>Qui puoi vedere quanti ti hanno contattato nell'ultimo mese : {{-- Inserimento dinamico di messaggi ricevuti nell'ultimo mese Mese Corrente - Inizio Mese per il calcolo --}}</li>
-            @php
-                $to_read_count = 0;
-                foreach ($messages as $item) {
-                    if($item->read === 0){
-                        $to_read_count++;
-                    }
+    <div class="border border-1 w-1/2 max-sm:w-full p-4">
+        <p class="text-center">Vediamo quanti messaggi hai ricevuto : <strong>{{ $messages->count() }}</strong>
+            {{-- Inserimento dati dinamici dei messaggi ricevuti --}}</p>
+        @php
+            $to_read_count = 0;
+            foreach ($messages as $item) {
+                if ($item->read === 0) {
+                    $to_read_count++;
                 }
-            @endphp
-            <li>Hai <i> {{$to_read_count}} </i> {{-- Numero di messaggi con isRead a false --}}Messaggi non letti ! </li>
-        </ul>
+            }
+        @endphp
+        @if ($to_read_count !== 0)
+            <p class="mt-4">Hai <i> {{ $to_read_count }} </i> {{-- Numero di messaggi con isRead a false --}}Messaggi non @if ($to_read_count > 1)
+                    letti ! </p>
+        @else
+            letto!
+        @endif
+    @else
+        <p class="mt-4">Non hai nessun messaggio non letto !</p>
+        @endif
+        @php
+            /* $last_month_msg = 0;
+                $curMonth = date('m');
+                for($i = 0; $i < $messages->count() - 1; $i++){
+                    $formattedMonth = $messages[$i]->created_at->format('m');
+                    $curMonth === $formattedMonth ? $last_month_msg++ : continue;
+                } */
+            /* Versione ottimizzata */
+            $current_month = now()->format('m');
+            $last_month_msgs = $messages
+                ->filter(function ($message) use ($current_month) {
+                    return $message->created_at->format('m') === $current_month;
+                })
+                ->count();
+        @endphp
+        <p>Nell'ultimo mese ti hanno contatta : {{ $last_month_msgs }} volte!{{-- Inserimento dinamico di messaggi ricevuti nell'ultimo mese Mese Corrente - Inizio Mese per il calcolo --}}</p>
     </div>
     {{-- / Welcome --}}
 @endsection
--- obj_selfie-back-end/resources/views/layouts/app.blade.php
+++ obj_selfie-back-end/resources/views/layouts/app.blade.php
@@ -11,10 +11,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -25,22 +23,28 @@
     @endphp
     <nav class="bg-slate-500">
         <ul class="flex justify-center items-center gap-6 p-4 text-white">
-            <li><a href="{{ route('admin.dashboard')}}" class="{{ $currentPath === "admin" ? 'italic' : 'opacity-50'}} hover:underline underline-offset-8">Dashboard</a></li>
-            <li><a href="{{ route('admin.messages.index')}}"  class="{{ $currentPath === "admin/messages" ? 'italic' : 'opacity-50'}} hover:underline underline-offset-8">Messaggi</a></li>
+            <li><a href="{{ route('admin.dashboard') }}"
+                    class="{{ $currentPath === 'admin' ? 'italic' : 'opacity-50' }} hover:underline underline-offset-8">Dashboard</a>
+            </li>
+            <li><a href="{{ route('admin.messages.index') }}"
+                    class="{{ $currentPath === 'admin/messages' ? 'italic' : 'opacity-50' }} hover:underline underline-offset-8">Messaggi</a>
+            </li>
             {{-- Non posso usare collegamento diretto in quanto route() fa un GET 
             <li><a href="{{ route('logout')}}">Logout</a></li> 
             --}}
             <li>
-                <form action="{{route('logout')}}" method="post">
+                <form action="{{ route('logout') }}" method="post">
                     @csrf
-                    <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent hover:transition rounded-xl transition" type="submit">Logout</button>
+                    <button
+                        class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent hover:transition rounded-xl transition"
+                        type="submit">Logout</button>
                 </form>
             </li>
         </ul>
 
 
     </nav>
-    <main class="flex max-sm:flex-col">
+    <main class="flex flex-col gap-6 items-center mt-10">
         @yield('content')
     </main>
 </body>
