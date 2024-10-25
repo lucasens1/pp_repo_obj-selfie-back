dbeb8f76a7b708221a4571c5cde77bfe9afbd126
Luca Sensini <157043499+lucasens1@users.noreply.github.com>
2024-10-25 18:17:16 +0200

    styling and updating

--- obj_selfie-back-end/resources/views/admin/messages/index.blade.php
+++ obj_selfie-back-end/resources/views/admin/messages/index.blade.php
@@ -1,41 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col justify-center w-full text-center">
-        <h1>Messaggi!</h1>
+    <div class="w-full text-center">
+        <h1 class="text-2xl mb-12 font-semibold">Messaggi!</h1>
         @if (session('success'))
-            <div class="border-1 border-green-500 text-green-600">
-                {{ session('success')}}
+            <div class="border-1 border-green-500 text-green-600 my-6 p-2">
+                {{ session('success') }}
             </div>
         @endif
-        <table class="border-2">
-            <thead>
-                <tr>
-                    {{-- <th>ID</th> --}}
-                    {{-- <th>Nome</th> --}}
-                    <th>Email</th>
-                    {{-- <th>Telefono</th> --}}
-                    <th>Oggetto</th>
-                    {{-- <th>Messaggio</th> --}}
-                    <th>Letto?</th>
-                </tr>
-            </thead>
 
-            <tbody>
-                @foreach ($messages as $item)
-                    <tr>
-                        {{-- <td> {{ $item->id }}</td> --}}
-                        {{-- <td> {{ $item->name }}</td> --}}
-                        <td> {{ $item->email }}</td>
-                        {{-- <td> {{ $item->phone}} </td> --}}
-                        <td> {{ $item->subject}}</td>
-                        {{-- <td> {{ $item->message }}</td> --}}
-                        <td> <span class="bg-red-500 text-white rounded-3xl p-2 block">{{ $item->read ? '✔' : '✘'}}</span> </td>
-                        <td> <a href="{{route('admin.messages.show', $item->id)}}" class="bg-transparent hover:bg-blue-500 text-black font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent hover:transition rounded-xl transition">Dettagli</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
+        <div class="mb-4">
+            <select name="tovisual" id="tovisual" onchange="filter()">
+                <option default value="all">Tutti</option>
+                <option value="toread">Da Leggere</option>
+                <option value="read">Letti</option>
+            </select>
+        </div>
 
-        </table>
+        <div class="flex max-sm:w-full flex-wrap justify-center gap-4 " id="messageContainer">
+            @foreach ($messages as $message)
+                {{-- Aggiungo data-status per controllare lo status del messaggio e nasconderlo all'occorrenza --}}
+                <div class="max-w-sm rounded-2xl overflow-hidden shadow-2xl h-full p-4 {{ $message->read ? 'bg-slate-700 text-white' : '' }}"
+                    data-status="{{ $message->read ? 'read' : 'toread' }}">
+                    <div class="px-3">
+                        <div class="font-bold text-xl mb-2">{{ $message->subject }}</div>
+                        <p class="text-md">
+                            {{ $message->message }}
+                        </p>
+                    </div>
+                    <div class="p-2">
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-md font-semibold text-gray-700 mr-2 mb-2">{{ $message->name }}</span>
+                    </div>
+                    <div class="p-2">
+                        <button class="bg-orange-400 px-3 py-4 text-sm text-white rounded-3xl hover:bg-orange-600"><a
+                                href="{{ route('admin.messages.show', $message->id) }}">Dettagli Messaggio</a></button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
+
+    <script>
+        function filter() {
+            const selectedFilter = document.getElementById('tovisual').value; /* Prendo valore selezionato da User */
+            const messages = document.querySelectorAll(
+            '#messageContainer > div'); /* Prendo il div contenuto nel container di Tutti i Msgs */
+            messages.forEach(elem => {
+                if (selectedFilter === 'all' || elem.getAttribute('data-status') === selectedFilter) {
+                    elem.style.display = 'block';
+                } else {
+                    elem.style.display = 'none';
+                }
+            });
+        }
+    </script>
 @endsection
--- obj_selfie-back-end/resources/views/admin/messages/show.blade.php
+++ obj_selfie-back-end/resources/views/admin/messages/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col gap-4 p-4">
-        <h2>Dettaglio messaggio!</h2>
+    <div class="flex flex-col gap-4 p-6 border-4 border-orange-400">
+        <h2 class="text-lg font-semibold">Dettaglio messaggio!</h2>
         <p><strong>Nome:</strong> {{ $message->name }}</p>
-        <p><strong>Email:</strong> {{ $message->email }}</p>
+        <p onclick="copyEmail('{{ $message->email }}')" style="cursor: pointer;"><strong>Email:</strong> {{ $message->email }}</p>
         <p><strong>Telefono:</strong> {{ $message->phone }}</p>
         <p><strong>Oggetto:</strong> {{ $message->subject }}</p>
         <p><strong>Messaggio:</strong> {{ $message->message }}</p>
         <div class="flex gap-4">
-            <button class="bg-orange-500 rounded-2xl text-white p-2 block">
-                {{-- <a
-                    target="_blank"
-                    href="https://mail.google.com/mail/?view=cm&fs=1&to={{ $message->email }}&su=RE : {{ urlencode($message->subject) }}&body={{ urlencode($message->message) }}">
-                    Invia Email
-                </a> --}}
-            </button>
             <form action="{{ route('admin.messages.destroy', $message->id) }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="bg-red-500 text-white rounded-3xl p-2 block">Cancella Messaggio</button>
             </form>
         </div>
-
     </div>
+    <script>
+        function copyEmail(email) {
+            navigator.clipboard.writeText(email)
+                .then(() => {
+                    alert("Email copiata!");
+                })
+                .catch(err => {
+                    console.error("Errore nella copia: ", err);
+                });
+        }
+    </script>
 @endsection
